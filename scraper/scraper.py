cb612ec4620c231e4cb31ce07b5eacffd3b59468
thisisyoojin <thisisyoojin@gmail.com>
2022-01-22 22:56:59 +0000

    Base scraper is added

---
+++ scraper/scraper.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+import time
+import requests
+
+
+class Scraper:
+    """
+    Scraper base class. Other scraper will be inherited from this class.
+
+    Init Params
+    ROOT_DOMAIN: domain address to scrap
+    option: firefox option
+    
+    Method
+    self.start(): start firefox webdriver
+    self.get_page_by_url(url, time): browse to url and wait for given time (default:5 sec)
+    self.download_img_file(src_url, fpath): save image file from source url to filepath
+    self.close(): close firfox webdriver
+    """
+
+    def __init__(self, ROOT_DOMAIN, option=None):
+        self.ROOT_DOMAIN = ROOT_DOMAIN
+        self.option = option
+        self.driver = None
+
+    def start(self):
+        self.driver = webdriver.Firefox(self.option)
+
+    def get_page_by_url(self, url, time=5):
+        self.driver.get(url)
+        time.sleep(time)
+
+
+    def download_img_file(self, src_url, fpath):
+        response = requests.get(src_url)
+        with open(fpath, 'wb+') as f:
+            f.write(response.content)
+
+
+    def close(self):
+        self.driver.close()
+
+
